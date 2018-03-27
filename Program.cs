c697c0b5907783447944ff68fa9e28b8e74b99b5
Evan Schiewe <supernova190@gmail.com>
2018-03-27 01:18:12 -0500

    Format test output.

--- Program.cs
+++ Program.cs
@@ -12,34 +12,37 @@ namespace entegra
 
             BusinessCardParser b = new BusinessCardParser();
             string[] test = {
-                @"Entegra Systems
-                John Doe
-                Senior Software Engineer
-                (410)555-1234
-                john.doe@entegrasystems.com",
-                @"Acme Technologies
-                Analytic Developer
-                Jane Doe
-                1234 Roadrunner Way
-                Columbia, MD 12345
-                Phone: 410-555-1234
-                Fax: 410-555-4321
-                Jane.doe@acmetech.com",
-                @"Bob Smith
-                Software Engineer
-                Decision & Security Technologies
-                ABC Technologies
-                123 North 11th Street
-                Suite 229
-                Arlington, VA 22209
-                Tel: +1 (703) 555-1259
-                Fax: +1 (703) 555-1200
-                bsmith@abctech.com"
+@"Entegra Systems
+John Doe
+Senior Software Engineer
+(410)555-1234
+john.doe@entegrasystems.com",
+@"Acme Technologies
+Analytic Developer
+Jane Doe
+1234 Roadrunner Way
+Columbia, MD 12345
+Phone: 410-555-1234
+Fax: 410-555-4321
+Jane.doe@acmetech.com",
+@"Bob Smith
+Software Engineer
+Decision & Security Technologies
+ABC Technologies
+123 North 11th Street
+Suite 229
+Arlington, VA 22209
+Fax: +1 (703) 555-1200
+Tel: +1 (703) 555-1259
+bsmith@abctech.com"
             };
             foreach (string t in test)
             {
+                Console.WriteLine("Input: \n\n" + t);
+                Console.WriteLine("\n==>\n");
                 ContactInfo c = b.GetContactInfo(t);
                 c.print();
+                Console.WriteLine("\n==>\n");
             }
         }
     }
