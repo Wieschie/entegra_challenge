9e49cbbce5998578d79450597cd6158c59015358
Evan Schiewe <supernova190@gmail.com>
2018-03-27 00:07:06 -0500

    WIP name recognition with Stanford CoreNLP.
    Currently does full debug print of probabilities.

--- BusinessCardParser.cs
+++ BusinessCardParser.cs
@@ -1,7 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using edu.stanford.nlp.ie.crf;
+using edu.stanford.nlp.ling;
+using java.util;
 
 namespace entegra
 {
@@ -22,11 +25,62 @@ namespace entegra
         /// </returns>
         public ContactInfo GetContactInfo(string document)
         {
+            string name = extractName(ref document);
             string email = extractEmail(ref document);
             string phone = extractPhone(ref document);
-            return new ContactInfo("Evan", phone, email);
+            return new ContactInfo(name, phone, email);
         }
 
+        // note: SLF4J warning is a known issue and does not adversely affect anything
+        // https://github.com/sergey-tihon/Stanford.NLP.NET/issues/79
+        CRFClassifier nameClassifier = CRFClassifier.getClassifierNoExceptions(
+            @"english.all.3class.distsim.crf.ser.gz");
+        private string extractName(ref string document)
+        {
+            StringBuilder name = new StringBuilder();
+
+            // Split document into individual lines.  Linguistic context isn't important on a business card
+            // and may actually confuse the classifier.
+            string[] lines = document.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            foreach (string line in lines)
+            {
+                List labels = nameClassifier.classify(line);
+
+                foreach (List sentence in labels.toArray())
+                {
+                    CRFCliqueTree cliqueTree = nameClassifier.getCliqueTree(sentence);
+                    Array sentenceArr = sentence.toArray();
+                    //foreach (CoreLabel word in sentence.toArray())
+                    for (int i = 0; i < sentenceArr.Length; i++)
+                    {
+                        CoreLabel word = (CoreLabel) sentenceArr.GetValue(i);
+                        Console.Write("{0}/{1} ", word.word(), word.get(new CoreAnnotations.AnswerAnnotation().getClass()));
+                        if ((string)word.get(new CoreAnnotations.AnswerAnnotation().getClass()) == "PERSON")
+                        {
+                            if (name.Length > 0)
+                                name.Append(" ");
+                            name.Append(word.word());
+                        }
+                        Console.WriteLine('\n');
+
+                        for (Iterator iter = nameClassifier.classIndex.iterator(); iter.hasNext();)
+                        {
+                            String l = (string)iter.next();
+                        //string l = "PERSON";
+                            int index = nameClassifier.classIndex.indexOf(l);
+                            double probability = cliqueTree.prob(i, index);
+                            Console.WriteLine("\t" + l + "(" + probability + ")");
+                        }
+                    }
+
+                    Console.WriteLine('\n');
+                }
+            }
+            return name.ToString();
+        }
+
+
+        // regexes stored as members so they are compiled once and kept in memory for repeated use.
         // matches all valid US numbers with capture groups for each segment (4 total)
         //                                    optionally match US country code
         //                                    |         parentheses are optional
@@ -35,7 +89,9 @@ namespace entegra
         //                                    |          |   |               |             
         private static string phoneFormat = @"(1?)[-. ]?\(?([1-9][\d]{2})\)?[-. ]?([\d]{3})[-. ]?([\d]{4})";
         private static Regex phoneRegex = new Regex(phoneFormat, RegexOptions.Compiled);
-        private string extractPhone(ref string document) {
+
+        private string extractPhone(ref string document)
+        {
             MatchCollection matches = phoneRegex.Matches(document);
             if (matches.Count == 0)
             {
@@ -46,22 +102,23 @@ namespace entegra
         }
 
 
-        // compile regex once to avoid constructing full NFA every time a search is performed.
-        private static string emailFormat = @"[\w\d%+-]+@[A-Z\d.-]+\.[A-Z]{2,}";
+        private static string emailFormat = @"[\w\d%+-.]+@[A-Z\d.-]+\.[A-Z]{2,}";
         private static Regex emailRegex = new Regex(emailFormat, RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-        private string extractEmail(ref string document) {
+        private string extractEmail(ref string document)
+        {
             MatchCollection matches = emailRegex.Matches(document);
             if (matches.Count == 0)
             {
                 throw new ArgumentException("Valid email not found.");
             }
             // @TODO: handle this in-function?
-            if (matches.Count > 1) {
+            if (matches.Count > 1)
+            {
                 throw new ArgumentException("Multiple emails found.");
             }
 
-            return matches[0].Value;
+            return matches[0].Value.ToLower();
         }
 
     }
